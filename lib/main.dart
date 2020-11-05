a16dd6009a9285e82007351c4eb25ec7ab9c3345
ankitshah86 <ankit.r.shah63@gmail.com>
2020-11-04 22:27:20 -0500

    stateless image tab ui implemented

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
           body: TabBarView(
             children: [
               SurveyPage(),
-              Icon(Icons.access_alarm),
+              ImageTab(),
               Icon(Icons.accessible),
               Icon(Icons.foundation),
             ],
@@ -100,6 +100,26 @@ class SurveyPage extends StatelessWidget {
           ),
         )
       ]),
+    );
+  }
+}
+
+class ImageTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Column(
+        children: [
+          //Image(image: AssetImage("images/tea.jpg")),
+          Padding(
+            padding: EdgeInsets.all(90),
+            child: Image(
+              image: AssetImage("images/tea.jpg"),
+            ),
+          ),
+          Text("Morning Tea")
+        ],
+      ),
     );
   }
 }
