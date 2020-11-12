bc69e954f409c4be501a29beed3b44daa40c74e8
ankitshah86 <ankit.r.shah63@gmail.com>
2020-11-11 23:14:42 -0500

    gradient button added in survey page

--- lib/surveyTab.dart
+++ lib/surveyTab.dart
@@ -19,39 +19,63 @@ class _SurveyPageState extends State<SurveyPage> {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: new Column(children: <Widget>[
-        Container(
-          child: Text(
-            "This is a survey question This is a survey question This is a survey question This is a survey question This is a survey question This is a survey question?",
+      padding: EdgeInsets.all(20),
+      child: new Column(
+        children: <Widget>[
+          Container(
+            child: Text(
+              "This is a survey question This is a survey question This is a survey question This is a survey question This is a survey question This is a survey question?",
+            ),
+            padding: const EdgeInsets.all(20.0),
           ),
-          padding: const EdgeInsets.all(20.0),
-        ),
-        ListTile(
-          title: const Text("option A"),
-          leading: Radio(
-            value: options.A,
-            autofocus: true,
-            groupValue: _myoption,
-            onChanged: _optionSelected,
+          ListTile(
+            title: const Text("option A"),
+            leading: Radio(
+              value: options.A,
+              autofocus: true,
+              groupValue: _myoption,
+              onChanged: _optionSelected,
+            ),
           ),
-        ),
-        ListTile(
-          title: const Text("option B"),
-          leading: Radio(
-            value: options.B,
-            groupValue: _myoption,
-            onChanged: _optionSelected,
+          ListTile(
+            title: const Text("option B"),
+            leading: Radio(
+              value: options.B,
+              groupValue: _myoption,
+              onChanged: _optionSelected,
+            ),
           ),
-        ),
-        ListTile(
-          title: const Text("option C"),
-          leading: Radio(
-            value: options.C,
-            groupValue: _myoption,
-            onChanged: _optionSelected,
+          ListTile(
+            title: const Text("option C"),
+            leading: Radio(
+              value: options.C,
+              groupValue: _myoption,
+              onChanged: _optionSelected,
+            ),
           ),
-        )
-      ]),
+          RaisedButton(
+            onPressed: () {},
+            textColor: Colors.white,
+            padding: EdgeInsets.all(0.0),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.blue,
+                    Colors.green,
+                  ],
+                ),
+              ),
+              padding: EdgeInsets.all(10.0),
+              child: const Text(
+                "Hello",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
